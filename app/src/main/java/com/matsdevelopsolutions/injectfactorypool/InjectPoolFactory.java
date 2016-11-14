71397edd74afe60899b87328fc34d080bad89228
Mateusz Perlak <mat.perlak@contextmediainc.com>
2016-11-14 06:54:21 -0600

    chage for view map stored pool

--- app/src/main/java/com/matsdevelopsolutions/injectfactorypool/InjectPoolFactory.java
+++ app/src/main/java/com/matsdevelopsolutions/injectfactorypool/InjectPoolFactory.java
@@ -22,9 +22,18 @@ public class InjectPoolFactory implements LayoutInflater.Factory2 {
 
     private final LayoutInflater.Factory2 fallbackInflaterFactory;
     // pool per type with list
-    private final Map<String, List<View>> freeViewPool = new ConcurrentHashMap<>();
-    private final Map<String, List<View>> inUseViewPool = new ConcurrentHashMap<>();
+    private final Map<String, List<ViewDetails>> freeViewPool = new ConcurrentHashMap<>();
+    private final Map<String, List<ViewDetails>> inUseViewPool = new ConcurrentHashMap<>();
 
+    public static class ViewDetails {
+        public ViewDetails(String name, View view) {
+            this.name = name;
+            this.view = view;
+        }
+        public String name;
+        public View view;
+        public View.OnAttachStateChangeListener onAttachStateChangeListener;
+    }
     public InjectPoolFactory(LayoutInflater.Factory2 fallbackInflater) {
         this.fallbackInflaterFactory = fallbackInflater;
     }
@@ -43,61 +52,75 @@ public class InjectPoolFactory implements LayoutInflater.Factory2 {
         }
         return fallbackInflaterFactory.onCreateView(parent, name, context, attrs);
     }
-
     public void releaseView(View v) {
-        String name = (String) v.getTag(R.id.inject_tag);
+//        if (inUseViewPool.values().contains(v)) {
+//            for(Map.Entry<String, List<ViewDetails>> entry : inUseViewPool.entrySet()){
+//                if (entry.getValue().contains(v).equals(v)) {
+//                    releaseView(v);
+//                }
+//            }
+//        }
+    }
+    public void releaseView(ViewDetails v) {
+        String name = (String) v.name;
         if (name != null) {
-            List<View> viewList = inUseViewPool.get(name);
+            List<ViewDetails> viewList = inUseViewPool.get(name);
+            // find View in list of view details
             if (viewList.contains(v)) {
                 viewList.remove(v);
-                List<View> poolList = freeViewPool.get(name);
+                List<ViewDetails> poolList = freeViewPool.get(name);
                 if (poolList == null) {
                     poolList = new ArrayList<>();
+                    freeViewPool.put(name, poolList);
                 }
                 poolList.add(v);
 
-                //cleanUpView(v);
+             //   cleanUpView(v);
             }
         }
     }
 
-    private View fetchView(View parent, String name, Context context, AttributeSet attrs) {
-        List<View> viewList = freeViewPool.get(name);
+    private ViewDetails fetchView(View parent, String name, Context context, AttributeSet attrs) {
+            List<ViewDetails> viewList = freeViewPool.get(name);
         if (viewList != null && viewList.size() > 0) {
-            View viewFromPool = freeViewPool.get(name).get(0);
+            ViewDetails viewFromPool = freeViewPool.get(name).get(0);
             cleanUpView(viewFromPool);
 
             return viewFromPool;
         } else {
-            return fallbackInflaterFactory.onCreateView(parent, name, context, attrs);
+            ViewDetails vd = new ViewDetails(name, fallbackInflaterFactory.onCreateView(parent, name, context, attrs));
+            return vd;
         }
     }
 
     @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR1)
     private View onCreateViewInternal(View parent, String name, Context context, AttributeSet attrs) {
 
-        View view = fetchView(parent, name, context, attrs);
+        ViewDetails view = fetchView(parent, name, context, attrs);
         if (view != null) {
-            view.addOnAttachStateChangeListener(new View.OnAttachStateChangeListener() {
-                @Override
-                public void onViewAttachedToWindow(View v) {
+            if (view.onAttachStateChangeListener == null) {
+                view.onAttachStateChangeListener = new View.OnAttachStateChangeListener() {
+                    @Override
+                    public void onViewAttachedToWindow(View v) {
 
-                }
+                    }
 
-                @Override
-                public void onViewDetachedFromWindow(View v) {
-                    releaseView(v);
-                }
-            });
+                    @Override
+                    public void onViewDetachedFromWindow(View v) {
+                        releaseView(v);
+                    }
+                };
+                view.view.addOnAttachStateChangeListener(view.onAttachStateChangeListener);
+            }
             addToPool(name, view);
-            return view;
+            return view.view;
         }
         return null;
     }
 
-    private void addToPool(String name, View view) {
-        view.setTag(R.id.inject_tag, name);
-        List<View> viewList = inUseViewPool.get(name);
+    private void addToPool(String name, ViewDetails view) {
+        view.name = name;
+        List<ViewDetails> viewList = inUseViewPool.get(name);
         if (viewList != null) {
             viewList.add(view);
         } else {
@@ -107,12 +130,13 @@ public class InjectPoolFactory implements LayoutInflater.Factory2 {
         }
     }
 
-    private void cleanUpView(View v) {
+    private void cleanUpView(ViewDetails v) {
+
         // remove parent
-        ViewParent parent = v.getParent();
-        if (parent != null && parent instanceof ViewGroup) {
-            ((ViewGroup) parent).removeView(v);
-        }
+//        ViewParent parent = v.getParent();
+//        if (parent != null && parent instanceof ViewGroup) {
+//            ((ViewGroup) parent).removeView(v);
+//        }
         // todo default styles reset
     }
 }
